403ca5f644dd4d03a2d0e54c9e43deca49c06edd
some-earth11 <some_earth@outlook.com>
2021-10-05 06:31:20 +0530

    Button Set Up

--- gui_launcher.py
+++ gui_launcher.py
@@ -4,7 +4,7 @@ from tkinter import *
 from tkinter import messagebox, filedialog
 
 # Import Functions from main.py
-from main import *
+#import main
 
 # Creating object of tk class
 root = tk.Tk()
@@ -15,3 +15,28 @@ root.resizable(False, False)
 root.iconbitmap('petrol.ico')    
 root.title("Petrol Pump Finder")
 root.config(background="white")
+
+# Setting up Image for Button
+photo = PhotoImage(file = r'Red_button.png')
+
+def Widgets():
+    head_label = Label(root,
+                    text="FIND ME A PUMP!",
+					padx=15,
+					pady=15,
+					font="Arial 18",
+					bg="black",
+					fg="white")
+    head_label.config(anchor=CENTER)
+    head_label.pack()
+
+    button = Button(root,
+                    text="Click Me!",
+                    image = photo)
+    button.config(anchor=CENTER)
+    button.pack()
+
+# Calling all Widgets into Action
+Widgets()
+# Infinite Loop for the Program
+root.mainloop()
