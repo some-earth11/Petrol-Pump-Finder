40b21aa7f0ee8b4438cf6203bfac45110f285248
some-earth11 <some_earth@outlook.com>
2021-10-05 06:42:56 +0530

    Fixed Compatibility with GUI Launcher

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from selenium import webdriver
 # Setting up Web Driver
 driver = webdriver.Chrome(r"C:\Users\some_\chromedriver.exe")   # Address of the Driver Executable
 
-driver.get("https://www.google.com/maps")
-time.sleep(2)
 
 # Search for Nearest Petrol Pump
 def searchplace():
+    driver.get("https://www.google.com/maps")
+    time.sleep(2)
     Place = driver.find_element_by_class_name("tactile-searchbox-input")
     Place.send_keys("petrol pump near me")
     Submit = driver.find_element_by_xpath('//*[@id="searchbox-searchbutton"]')
