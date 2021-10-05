d93faeb5fd5798f6022d329c149da16704d7b874
some-earth11 <some_earth@outlook.com>
2021-10-05 06:11:03 +0530

    Tkinter Box SetUp

--- gui_launcher.py
+++ gui_launcher.py
@@ -6,3 +6,12 @@ from tkinter import messagebox, filedialog
 # Import Functions from main.py
 from main import *
 
+# Creating object of tk class
+root = tk.Tk()
+
+# Setting up the Box 
+root.geometry("720x480")                      
+root.resizable(False, False)
+root.iconbitmap('petrol.ico')    
+root.title("Petrol Pump Finder")
+root.config(background="white")
